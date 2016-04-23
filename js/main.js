a402ec3ce05ed9768bfb114075a11f75b2fe9aed
Matko Jun <matko.jun@gmail.com>
2016-04-23 17:53:21 +0200

    Basic design and angularjs logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,110 @@
-/**
-* Main AngularJS Web Application
-*/
+var app = angular.module('lowCostFlightsWebApp', ['bootstrap3-typeahead']);
 
-var app = angular.module('lowCostFlightsWebApp', []);
+app.factory("lowCostFlightsService", ['$http', '$q', function($http, $q) {
+    return {
+        search: function(params) {
+            return $http.get('http://digress.me:8080/search', params)
+                .then(
+                    function(response) {
+                        return response.data;
+                    },
+                    function(errResponse) {
+                        console.error('Error while performing search');
+                        return $q.reject(errResponse);
+                    }
+                );
+        },
+        getAirport: function(iata_code) {
+            return $http.get('http://digress.me:8080/airport/' + iata_code)
+                .then(
+                    function(response) {
+                        return response.data;
+                    },
+                    function(errResponse) {
+                        console.error('Error while trying to get airport');
+                        return $q.reject(errResponse);
+                    }
+                );
+        },
+        findAirports: function(name) {
+            return $http.get('http://digress.me:8080/airport/find?name=' + name)
+                .then(
+                    function(response) {
+                        return response.data;
+                    },
+                    function(errResponse) {
+                        console.error('Error while trying to find airports');
+                        return $q.reject(errResponse);
+                    }
+                )
 
-app.controller("AirportController", [ '$scope', '$resource',
-		function($scope, $resource) {
-			//
-			// GET Action Method
-			//
-			var Airport = $resource('http://digress.me:8080/airport/ZAG', {});
-			Airport.get( {}, function(airport){
-				$scope.airport = airport;
-        console.log(airport);
-			})
+        }
 
-		} ]);
+    }
+}]);
+
+app.controller("flightSearchController", ['$scope', 'lowCostFlightsService', function($scope, lowCostFlightsService) {
+    $scope.airports = [];
+    $scope.flights = [];
+		$scope.params = {
+			origin: null,
+			destination: null,
+			departure: null,
+			return: null,
+			adults: null,
+			children: null,
+			infants: null,
+			currency: null
+		};
+
+    $scope.airportDisplayText = function(item) {
+        return "(" + item.iata_code + ") " + item.name;
+    };
+
+    $scope.afterOriginAirportSelect = function(item) {
+        $scope.params.origin = item.iata_code;
+    };
+
+    $scope.afterDestinationAirportSelect = function(item) {
+        $scope.params.destination = item.iata_code;
+    };
+
+    $scope.findAirports = function(name) {
+        if (name.length >= 3) {
+            if (name.length == 3 && name == name.toUpperCase()) {
+                lowCostFlightsService.getAirport(name).then(
+                    function(response) {
+                        //$scope.airports = [response];
+												console.log("LOG: " + JSON.stringify(response));
+                    },
+                    function(errResponse) {
+                        console.error("No airport with IATA code " + name);
+                    }
+                );
+            } else {
+                lowCostFlightsService.findAirports(name).then(
+                    function(response) {
+                        //$scope.airports = response;
+												console.log("LOG: " + JSON.stringify(response));
+                    },
+                    function(errResponse) {
+                        console.error('Error while fetching airports');
+                    }
+                );
+            }
+
+        }
+    };
+
+    $scope.search = function(params) {
+        lowCostFlightsService.search(params).then(
+            function(response) {
+                $scope.flights = response;
+            },
+            function(errResponse) {
+                console.error('Error while fetching flights');
+            }
+        );
+    };
+
+}]);
